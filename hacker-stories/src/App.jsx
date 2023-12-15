2e8fac6f80440718cd84578cd1629de73a569812
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2023-12-15 12:20:23 +1100

    try the map function to render multiple lists

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -2,12 +2,46 @@ import * as React from 'react';
 
 const title = 'React';
 
-function App(){
+const list = [
+  {
+    title:'React',
+    url:'https://reactjs.org/',
+    author:'Jordan Walke',
+    num_comments:3,
+    points:4,
+    objectID:0,
+  },
+  {
+    title:'Redux',
+    url:'https://redux.js.org/',
+    author:'Dan Abramov, Andrew Clark',
+    num_comments:2,
+    points:5,
+    objectID:1,
+  }
+]
+
+const App=()=>{
   return(
     <div>
-      <h1>Hello {title}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type = "text"/>
+      <hr/>
+      <ul>
+        {list.map((item)=>{
+          return(
+          <li key = {item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span><br/>
+            <span>{item.author}</span><br/>
+            <span>{item.num_comments}</span><br/>
+            <span>{item.points}</span>
+          </li>
+          ); }
+        )}
+      </ul>
     </div>
   );
 }
